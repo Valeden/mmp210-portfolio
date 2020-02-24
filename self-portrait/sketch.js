3f6e6b000e41b0c68c4d709f575e7e31a544e2c9
Valeden <49344334+Valeden@users.noreply.github.com>
2020-02-23 23:39:14 -0500

    Add files via upload

---
+++ self-portrait/sketch.js
@@ -0,0 +1,42 @@
+function setup() {
+  createCanvas(400, 400);
+  background('#DD8027');
+}
+
+function draw() {
+  fill('#E1B28C');
+  strokeWeight(2);
+  ellipse(200, 200, 260, 260);
+  fill('#FFFFFF');
+  strokeWeight(4);
+  rect(135, 160, 50, 30, 5);
+  rect(215, 160, 50, 30, 5);
+  fill('#000000');
+  rect(185, 160, 30, 5);
+  rect(75, 160, 60, 5);
+  rect(265, 160, 60, 5);
+  ellipse(80, 160, 40, 40);
+  ellipse(90, 140, 40, 40);
+  ellipse(100, 120, 40, 40);
+  ellipse(110, 100, 40, 40);
+  ellipse(130, 80, 40, 40);
+  ellipse(150, 70, 40, 40);
+  ellipse(150, 80, 40, 40);
+  ellipse(170, 70, 40, 40);
+  ellipse(175, 80, 40, 40);
+  ellipse(190, 70, 40, 40);
+  ellipse(195, 80, 40, 40);
+  ellipse(210, 70, 40, 40);
+  ellipse(215, 80, 40, 40);
+  ellipse(230, 70, 40, 40);
+  ellipse(235, 80, 40, 40);
+  ellipse(250, 70, 40, 40);
+  ellipse(250, 80, 40, 40);
+  ellipse(260, 70, 40, 40);
+  ellipse(280, 80, 40, 40);
+  ellipse(290, 100, 40, 40);
+  ellipse(300, 120, 40, 40);
+  ellipse(310, 140, 40, 40);
+  ellipse(320, 160, 40, 40);
+  rect(150, 250, 100, 10);
+}
